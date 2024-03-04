9ee02c6262a634c2f7adcadad804b19f0d323c1d
Jon Carter <jon.carter@lovellsports.co.uk>
2024-03-04 16:32:18 +0000

    Cal

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -69,7 +69,6 @@ async function getData() {
         const startDate = new Date(event.start.dateTime);
         const year = startDate.getFullYear();
         const month = startDate.getMonth() + 1; // Month is zero-indexed, so add 1
-        const monthYear = `${startDate.getFullYear()}${startDate.getMonth()}`;
 
         if (!eventsByYear[year]) {
           eventsByYear[year] = {};
