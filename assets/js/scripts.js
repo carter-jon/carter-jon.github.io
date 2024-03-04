2d8920164394add158d819c87aa89c950b96a2ac
Jon Carter <jon.carter@lovellsports.co.uk>
2024-03-04 15:19:30 +0000

    Cal

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -103,7 +103,7 @@ async function getData() {
 
           // Check if the month has events and is not in the past
           if (
-            // eventsByMonth[month] &&
+            eventsByMonth[month] &&
             // eventsByMonth[month].length > 0 &&
             (year > currentYear || (year === currentYear && month >= currentMonth))
           ) {
